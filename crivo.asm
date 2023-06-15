413405d6ca4eb131b8fc9b15ce675cf9075f3c04
Gabriel Freitas Willig <gwillig@pop-os.localdomain>
2023-06-15 16:45:18 -0300

    asm

--- crivo.asm
+++ crivo.asm
@@ -2,7 +2,7 @@
     MOVI R2, 1
 
 inicializa:
-    MOVI [R1], R1
+    MOVR [R1], R1
     ADDI R1, 1
     CMPI R1, 33
     JUMPR flag_n, inicializa
@@ -19,3 +19,15 @@ marca:
     ADDI R1, 1    
     CMPR [R1], R0
     JUMPR flag_nz, marca
+
+    MOVI R3, 21111 1001 
+    MOVI R5, 0
+loop:
+    MOVR R4, [R3]
+    ADDI R3, 1
+    CMPR R4, R0
+    JUMPR flag_z, loop
+    MOVR [R5], R4
+    ADDI R5, 1
+    CMPI R4, 33
+    JUMPR flag_n, loop
