0ab7cb7193656283f0f5baa53ed7637ae2e2efb3
Gabriel Freitas Willig <gwillig@pop-os.localdomain>
2023-06-15 17:05:07 -0300

    corrigido pseudo assembly

--- crivo.asm
+++ crivo.asm
@@ -1,21 +1,19 @@
-    MOVI R1, 2
-    MOVI R2, 1
+    MOV R1, #2
+    MOV R2, #1
 
-inicializa:
-    MOVI [R1], R1
-    ADDI R1, 1
-    CMPI R1, 33
-    JUMPR flag_n, inicializa
+    MOV [R1], R1
+    ADD R1, #1
+    CMP R1, #33
+    JUMPR N, #0x3
 
-    MOVI R1, 2
-marca:
-    MOVR R2, R1
-    ADDR R2, R1
-    marca2:
-        MOVR [R2], R0
-        ADDR R2, R1
-        CMPI R2, 33
-        JUMPR flag_n, marca2
-    ADDI R1, 1    
-    CMPR [R1], R0
-    JUMPR flag_nz, marca
+    MOV R1, #2
+
+    MOV R2, R1
+    ADD R2, R1
+    MOV [R2], R0
+    ADD R2, R1
+    CMP R2, #33
+    JUMPR N, #0x4
+    ADD R1, #1    
+    CMP R0, [R1]
+    JUMPR NZ, #0x8
